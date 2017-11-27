c1db22d1d3b07c7b89e3bb0dcf73543fa881968a
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2017-11-27 03:42:56 -0200

    Branch functions support

--- src/hocs/index.js
+++ src/hocs/index.js
@@ -1,7 +1,7 @@
 //  @flow
-import type { Node } from 'react'
+import * as React from 'react'
 import { branch, mapProps, renderComponent, compose, type HOC } from 'recompose'
-import { graphql } from 'react-apollo'
+import { graphql, type QueryProps } from 'react-apollo'
 import type { OperationOption } from 'react-apollo'
 
 export type FetchState = {
@@ -24,17 +24,17 @@ const activelyRefetching = (networkStatus: number): boolean => networkStatus ===
 const passivelyRefetching = (networkStatus: number): boolean => networkStatus === 2 || networkStatus === 6;
 const fetchingMore = (networkStatus: number): boolean => networkStatus === 3;
 
-export const withLoading = <Props: {}>(isLoading: (props: Props) => boolean, LoadingComponent: Node): HOC<Props, Props> => branch(
+export const withLoading = <Props: {}>(isLoading: (props: any) => boolean, LoadingComponent: React.ComponentType<Props>): HOC<Props, Props> => branch(
   isLoading,
   renderComponent(LoadingComponent),
 )
 
-export const withPlaceholder = <Props: {}>(isEmpty: (props: Props) => boolean, PlaceholderComponent: Node): HOC<Props, Props> => branch(
+export const withPlaceholder = <Props: {}>(isEmpty: (props: any) => boolean, PlaceholderComponent: React.ComponentType<Props>): HOC<Props, Props> => branch(
   isEmpty,
   renderComponent(PlaceholderComponent),
 )
 
-export const withError = <Props: {}>(isError: (props: Props) => boolean, ErrorComponent: Node): HOC<Props, Props> => branch(
+export const withError = <Props: {}>(isError: (props: any) => boolean, ErrorComponent: React.ComponentType<Props>): HOC<Props, Props> => branch(
   isError,
   renderComponent(ErrorComponent),
 )
@@ -57,16 +57,25 @@ export const withApolloFetchActions = <Props: {}>(): HOC<Props, FetchActions & P
   },
 }))
 
-type ComponentMap = {
-  LoadingComponent: Node,
-  PlaceholderComponent: Node,
-  ErrorComponent: Node
+type ComponentMap<Props> = {
+  LoadingComponent: React.ComponentType<Props>,
+  PlaceholderComponent: React.ComponentType<Props>,
+  ErrorComponent: React.ComponentType<Props>
 }
-export const enhancedGraphql = <Props: {}>(query: any, config: OperationOption<*, *>, componentMap: ComponentMap): HOC<Props, Props & FetchState & FetchActions> => compose(
+
+type BranchFunctions = {
+  isEmpty: (props: any) => boolean
+}
+
+const defaultBranchFunctions = {
+  isEmpty: ({ data }) => !data,
+}
+
+export const enhancedGraphql = <Props: {}>(query: any, config: OperationOption<*, *>, componentMap: ComponentMap<Props & FetchState & FetchActions>, branchFunctions: BranchFunctions): HOC<Props, Props & FetchState & FetchActions> => compose(
   graphql(query, config),
   withApolloFetchState(),
   withApolloFetchActions(),
-  withError(({ error }) => error, componentMap.ErrorComponent),
-  withLoading(({ data: { loading } }) => loading, componentMap.LoadingComponent),
-  withPlaceholder(({ data }) => !data, componentMap.PlaceholderComponent),
+  withError(({ error }) => !!error, componentMap.ErrorComponent),
+  withLoading(props => props.fetchState.initialLoading, componentMap.LoadingComponent),
+  withPlaceholder(branchFunctions.isEmpty || defaultBranchFunctions.isEmpty, componentMap.PlaceholderComponent),
 )
